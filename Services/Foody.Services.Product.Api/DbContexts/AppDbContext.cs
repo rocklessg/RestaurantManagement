f78bb923583fcdf427528b418f7910f64ecbab18
Abdulhafiz <adanmaigoro@fsdhgroup.com>
2024-01-26 21:21:18 +0100

    Implement Database configuration

---
+++ Services/Foody.Services.Product.Api/DbContexts/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Foody.Services.Product.Api.DbContexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option) : base(option)
+        {
+                
+        }
+    }
+}
