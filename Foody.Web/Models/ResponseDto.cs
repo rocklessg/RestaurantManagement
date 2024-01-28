177fc1ee8b28ac134710c9a6209ad948a2124ffc
Abdulhafiz <adanmaigoro@fsdhgroup.com>
2024-01-28 15:41:43 +0100

    Implement Generic Api call

---
+++ Foody.Web/Models/ResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Foody.Web.Models
+{
+    public class ResponseDto
+    {
+        [DefaultValue(true)]
+        public bool IsSuccess { get; set; } //= true;
+        public object Result { get; set; }
+        public string DisplayMessage { get; set; } = "";
+        public List<string> ErrorMessages { get; set; }
+    }
+}
