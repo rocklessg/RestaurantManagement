d2afd65c81e05044aebf31bd3e85b452e5576496
Abdulhafiz <adanmaigoro@fsdhgroup.com>
2024-01-28 09:49:32 +0100

    Implement Product Controller

--- Services/Foody.Services.Product.Api/Models/Dto/ResponseDto.cs
+++ Services/Foody.Services.Product.Api/Models/Dto/ResponseDto.cs
@@ -1,11 +1,8 @@
-﻿using System.ComponentModel;
-
-namespace Foody.Services.ProductApi.Models.Dto
+﻿namespace Foody.Services.ProductApi.Models.Dto
 {
     public class ResponseDto
     {
-        [DefaultValue("true")]
-        public bool IsSuccess { get; set; }
+        public bool IsSuccess { get; set; } = true;
         public object Result { get; set; }
         public string DisplayMessage { get; set; } = "";
         public List<string> ErrorMessages { get; set; }
