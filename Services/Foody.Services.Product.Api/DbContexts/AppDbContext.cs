5a2d72805eb34433e57dce829d035131475a1028
Abdulhafiz <adanmaigoro@fsdhgroup.com>
2024-01-28 08:04:51 +0100

    Create Product db

--- Services/Foody.Services.Product.Api/DbContexts/AppDbContext.cs
+++ Services/Foody.Services.Product.Api/DbContexts/AppDbContext.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Foody.Services.ProductApi.Models;
 
-namespace Foody.Services.Product.Api.DbContexts
+namespace Foody.Services.ProductApi.DbContexts
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> option) : base(option)
         {
-                
+
         }
+
+        public DbSet<Product> Products { get; set; }
+       // public DbSet<Product> Products { get; set; }
     }
 }
--- Services/Foody.Services.Product.Api/Models/Product.cs
+++ Services/Foody.Services.Product.Api/Models/Product.cs
@@ -1,6 +1,19 @@
-﻿namespace Foody.Services.Product.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Foody.Services.ProductApi.Models
 {
     public class Product
     {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Range(1, 1000)]
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
